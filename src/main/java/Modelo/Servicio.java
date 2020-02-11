dd7a0e38df2c96084d9d9d3f9b4ed01ab2103c7e
Jonnathan Matute <jmatutec2@gmail.com>
2020-02-11 13:20:24 -0500

    Index.html

--- src/main/java/Modelo/Servicio.java
+++ src/main/java/Modelo/Servicio.java
@@ -8,32 +8,37 @@ public class Servicio {
 
 	@Id
 	private int codigo;
+
 	private String descripción;
 	private double precio;
-	
+
 	public int getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(int codigo) {
 		this.codigo = codigo;
 	}
+
 	public String getDescripción() {
 		return descripción;
 	}
+
 	public void setDescripción(String descripción) {
 		this.descripción = descripción;
 	}
+
 	public double getPrecio() {
 		return precio;
 	}
+
 	public void setPrecio(double precio) {
 		this.precio = precio;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Servicio [codigo=" + codigo + ", descripción=" + descripción + ", precio=" + precio + "]";
 	}
-	
-	
+
 }
